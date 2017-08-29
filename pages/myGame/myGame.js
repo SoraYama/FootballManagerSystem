bad649ca387a9fd5307609366f3c7c9d3f22c265
yanqi.hou <yanqi.hou@anansimobile.org>
2017-08-29 23:12:07 +0800

    update to 8.29

---
+++ pages/myGame/myGame.js
@@ -0,0 +1,20 @@
+//logs.js
+var util = require('../../utils/util.js')
+let app = getApp()
+Page({
+  data: {
+    games: 0,
+  },
+  onLoad: function () {
+    app.getUserOpenId((err, openId) => {
+      if (err) {
+        console.error('Error', err)
+      } else {
+        console.log('openId: ', openId)
+        this.setData({
+          games: openId
+        })
+      }
+    })
+  }
+})
